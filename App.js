6d4bdf8145b9561696904322cb91e1dbcfa00b21
Eleni Katsareli <elenhkatsa@gmail.com>
2022-06-03 19:17:09 +0200

    added stylesheet

--- App.js
+++ App.js
@@ -1,27 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { colors, globalStyles } from './app/styles/globalStyles.js';
 import WelcomeScreen from './app/screens/WelcomeScreen.js';
 import LoginScreen from './app/screens/LoginScreen.js';
 import RegisterScreen from './app/screens/RegisterScreen.js';
-import CreateItem from './app/screens/CreateItem.js';
+import CreateItemScreen from './app/screens/CreateItemScreen.js';
 import MainPageScreen from './app/screens/MainPageScreen.js';
-
+import { useFonts } from 'expo-font';
+import { StyleSheet } from 'react-native';
 
 
 export default function App() {
+  let [fontsLoaded, error] = useFonts({
+    LatoRegular: require("./app/assets/fonts/Lato-Regular.ttf"),
+    LatoBold: require("./app/assets/fonts/Lato-Bold.ttf")
+  })
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text> Hi there beautiful girls! Let the coding begin </Text>
+    <View style={[globalStyles.container, globalStyles.body]}>
+      <Text > Hi there beautiful girls! Let the coding begin </Text>
+
+      <Text style={[globalStyles.text, styles.color]} > Hi there beautiful girls! Let the coding begin </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  color: {
+    backgroundColor: colors.light,
+    padding: 20
+  }
+})
+
