b0f1681d0e4ee9a35a5e94d27ffd5513cd1550f6
Eleni Katsareli <elenhkatsa@gmail.com>
2022-06-03 09:34:11 +0200

    added creareItemScreen

--- app/screens/CreateItemScreen.js
+++ app/screens/CreateItemScreen.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function CreateItem() {
+export default function CreateItemScreen() {
   return (
     <View>
       <Text>CreateItem</Text>
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function LoginScreen() {
+
   return (
     <View>
       <Text>LoginScreen</Text>
