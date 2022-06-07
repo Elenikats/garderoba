a1b302aa144c8e644be183e8e2983ff2e47068d2
Angela Herrig <angela.k.herrig@gmail.com>
2022-06-07 14:31:36 +0200

    created BottomTab

---
+++ app/screens/BottomTab.js
@@ -0,0 +1,87 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./HomeScreen.js";
+import FavoriteScreen from "./FavoriteScreen.js";
+import CameraScreen from "./CameraScreen.js";
+import ClosetScreen from "./ClosetScreen.js";
+import UserScreen from "./UserScreen.js";
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTab() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {},
+        tabBarActiveTintColor: "#27272A",
+        tabBarInactiveTintColor: "#C1C1C1",
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="tshirt" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name=" "
+        component={CameraScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <View style={styles.circle}>
+              <Icon name="camera" color="#FE5F10" size={40} />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Closet"
+        component={ClosetScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={UserScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  circle: {
+    zIndex: 3,
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: "lightgrey",
+    padding: 10,
+    margin: -10,
+    backgroundColor: "#F5F5F5",
+    shadowColor: "#27272A",
+    shadowOpacity: 0.25,
+    elevation: 5,
+    top: -10,
+  },
+});
