514a894a469467a9988cb3db5305a6f10c6ff04e
Angela Herrig <angela.k.herrig@gmail.com>
2022-06-08 11:30:14 +0200

    updates related to BottomTab.js

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen.js';
-import LoginScreen from './app/screens/LoginScreen.js';
-import RegisterScreen from './app/screens/RegisterScreen.js';
-import CreateItem from './app/screens/CreateItem.js';
-import MainPageScreen from './app/screens/MainPageScreen.js';
-
-
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import BottomTab from "./app/screens/BottomTab.js";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Hi there beautiful girls! Let the coding begin </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomTab />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
