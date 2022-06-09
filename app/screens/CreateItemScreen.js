aeedbf0d01a4005194b45beb9b65fadb69fe4264
Monika Ghimire <ghimire.monika00@gmail.com>
2022-06-09 11:26:04 +0200

    files modified

--- app/screens/CreateItemScreen.js
+++ app/screens/CreateItemScreen.js
@@ -1,10 +1,6 @@
 import react from "react";
 import { StyleSheet, SafeAreaView, Text, Image, PermissionsAndroid, TouchableOpacity, Button } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome5";
-import { Camera, CameraType } from 'expo-camera';
-import { useState, useRef, useEffect } from "react";
-import CameraPreview from "./CameraPreviewScreen";
-import * as ImagePicker from 'expo-image-picker';
 
 export default function CreateItemScreen() {
   return(
