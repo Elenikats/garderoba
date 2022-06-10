cb6c1a36e1ec3945a96bfcbfac0cd8f26b6a7e01
latifahal <latifahal@protonmail.com>
2022-06-10 09:27:15 +0200

    Login Layout finished

--- App.js
+++ App.js
@@ -1,27 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './app/screens/WelcomeScreen.js';
-import LoginScreen from './app/screens/LoginScreen.js';
+import LoginScreen from './app/screens/Login&Button/LoginScreen.js';
+import LoginButton from './app/screens/Login&Button/LoginButton.js';
 import RegisterScreen from './app/screens/RegisterScreen.js';
-import CreateItem from './app/screens/CreateItem.js';
+import CreateItemScreen from './app/screens/CreateItemScreen.js';
 import MainPageScreen from './app/screens/MainPageScreen.js';
 
 
 
+const Stack= createNativeStackNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Hi there beautiful girls! Let the coding begin </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+
+        {/* <Stack.Screen name="Main" component={BottomTab} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  //   <View style={{flex:1}}>
+  //     <LoginScreen />
+  //   </View>
+   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
