3c5879101f15750e5b7ac3b43e8cf3d2f4cce037
Eleni Katsareli <elenhkatsa@gmail.com>
2022-06-23 14:17:36 +0200

    ip

--- app/screens/CreateItemScreen.js
+++ app/screens/CreateItemScreen.js
@@ -14,7 +14,7 @@ import ColorPalette from "react-native-color-palette";
 import axios from "axios";
 import * as FileSystem from "expo-file-system";
 import { ImageBoxesContext } from "../../contexts/ImageBoxesContext.js";
-import * as Network from "expo-network";
+//import * as Network from "expo-network";
 import currentIP from "../utils/ip.js";
 
 export default function CreateItemScreen({ route, navigation }) {
@@ -59,7 +59,7 @@ export default function CreateItemScreen({ route, navigation }) {
     };
 
     // *********************** AXIOS ******************************+
-    // const ip = await Network.getIpAddressAsync();
+    //const ip = await Network.getIpAddressAsync();
     const ip = await currentIP();
     try {
       const response = await axios({
