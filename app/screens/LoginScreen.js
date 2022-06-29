c7c50866d0cfef145f664be19c3e711122a2ab75
Eleni Katsareli <elenhkatsa@gmail.com>
2022-06-29 14:24:49 +0200

    added images and styling in the favoriteScreen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -14,7 +14,6 @@ export default function LoginScreen({navigation}) {
   const [ email, setEmail ] = useState("");
   const [ password, setPassword ] = useState("");
   const [hidePassword, setHidePassword] = useState(true)
-  const [errors, setErrors] = useState([])
 
   const handleOpenEye = () => {
     setHidePassword(!hidePassword)
@@ -29,8 +28,8 @@ export default function LoginScreen({navigation}) {
     const url = `http://${ip}:9000/users/login`;
     try {
       const res = await axios.post(url, {
-        email: "angela.h@web.de",
-        password: "123456",
+        email: "cabbage@gmail.com",
+        password: "cabbage",
       });
       console.log("res data:", res.data);
       setUser(res.data.username);
